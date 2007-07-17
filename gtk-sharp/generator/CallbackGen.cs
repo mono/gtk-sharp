4554ebc55532acafbba7279268032921dea244db
Mike Kestner <mkestner@gmail.com>
2007-07-17 15:29:41 +0000

    2007-07-17  Mike Kestner  <mkestner@novell.com>
    	* generator/CallbackGen.cs : make GError** signatures fatal to
    	avoid compilation errors in gmime-sharp.
    
    svn path=/branches/gtk-sharp-2-468-branch/; revision=82107

--- gtk-sharp/generator/CallbackGen.cs
+++ gtk-sharp/generator/CallbackGen.cs
@@ -169,7 +169,7 @@ namespace GtkSharp.Generation {
 
 			if (cleanup_str != "")
 				sw.Write (cleanup_str);
-			bool fatal = (retval.MarshalType != "void" && retval.MarshalType != "bool") || has_out_params;
+			bool fatal = (retval.MarshalType != "void" && retval.MarshalType != "bool") || has_out_params || throws_error;
 			sw.WriteLine ("\t\t\t} catch (Exception e) {");
 			sw.WriteLine ("\t\t\t\tGLib.ExceptionManager.RaiseUnhandledException (e, " + (fatal ? "true" : "false") + ");");
 			if (fatal) {
