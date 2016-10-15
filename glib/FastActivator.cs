5de6ab8b20df0e5b8325f94fc3120e7debb5e5ff
Marius Ungureanu <marius.ungureanu@xamarin.com>
2016-10-15 18:59:36 +0300

    [Perf] Use different dictionaries and make fastactivator threadsafe
    This reduces chances of collisions on types, and also allows for a thread-safe access to the activation.

--- glib/FastActivator.cs
+++ glib/FastActivator.cs
@@ -9,46 +9,52 @@ namespace GLib
 	{
 		const BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.CreateInstance;
 
-		static readonly Dictionary<Type, Delegate> cache = new Dictionary<Type, Delegate> (new TypeEqualityComparer ());
 		delegate object FastCreateObjectPtr (IntPtr ptr);
-		static FastCreateObjectPtr FastCtorPtr (Type t)
+		static FastCreateObjectPtr FastCtorPtr (Type t, Dictionary<Type, FastCreateObjectPtr> cache)
 		{
-			Delegate method;
-			if (!cache.TryGetValue (t, out method)) {
-				var param = Expression.Parameter (typeof (IntPtr));
-				var newExpr = Expression.New (
-					t.GetConstructor (flags, null, new [] { typeof (IntPtr) }, new ParameterModifier [0]),
-					param
-				);
-				cache [t] = method = (FastCreateObjectPtr)Expression.Lambda (typeof (FastCreateObjectPtr), newExpr, param).Compile ();
+			FastCreateObjectPtr method;
+			lock (cache) {
+				if (!cache.TryGetValue (t, out method)) {
+					var param = Expression.Parameter (typeof (IntPtr));
+					var newExpr = Expression.New (
+						t.GetConstructor (flags, null, new [] { typeof (IntPtr) }, new ParameterModifier [0]),
+						param
+					);
+					cache [t] = method = (FastCreateObjectPtr)Expression.Lambda (typeof (FastCreateObjectPtr), newExpr, param).Compile ();
+				}
 			}
-			return (FastCreateObjectPtr)method;
+			return method;
 		}
 
 		delegate object FastCreateObject ();
-		static FastCreateObject FastCtor (Type t)
+		static FastCreateObject FastCtor (Type t, Dictionary<Type, FastCreateObject> cache)
 		{
-			Delegate method;
-			if (!cache.TryGetValue (t, out method)) {
-				var newExpr = Expression.New (t);
-				cache [t] = method = (FastCreateObject)Expression.Lambda (typeof (FastCreateObject), newExpr).Compile ();
+			FastCreateObject method;
+			lock (cache) {
+				if (!cache.TryGetValue (t, out method)) {
+					var newExpr = Expression.New (t);
+					cache [t] = method = (FastCreateObject)Expression.Lambda (typeof (FastCreateObject), newExpr).Compile ();
+				}
 			}
-			return (FastCreateObject)method;
+			return method;
 		}
 
+		static readonly Dictionary<Type, FastCreateObjectPtr> cacheOpaque = new Dictionary<Type, FastCreateObjectPtr> (new TypeEqualityComparer ());
 		public static Opaque CreateOpaque (IntPtr o, Type type)
 		{
-			return (Opaque)FastCtorPtr (type)(o);
+			return (Opaque)FastCtorPtr (type, cacheOpaque)(o);
 		}
 
+		static readonly Dictionary<Type, FastCreateObjectPtr> cacheObject = new Dictionary<Type, FastCreateObjectPtr> (new TypeEqualityComparer ());
 		public static Object CreateObject (IntPtr o, Type type)
 		{
-			return (Object)FastCtorPtr (type)(o);
+			return (Object)FastCtorPtr (type, cacheObject)(o);
 		}
 
+		static readonly Dictionary<Type, FastCreateObject> cacheSignalArgs = new Dictionary<Type, FastCreateObject> (new TypeEqualityComparer ());
 		public static SignalArgs CreateSignalArgs (Type type)
 		{
-			return (SignalArgs)FastCtor (type)();
+			return (SignalArgs)FastCtor (type, cacheSignalArgs)();
 		}
 
 		class TypeEqualityComparer : IEqualityComparer<Type>
