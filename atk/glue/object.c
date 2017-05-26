e13fd81ee5e1289ee944b894e843095f2ed99ea9
Marius Ungureanu <marius.ungureanu@xamarin.com>
2017-05-26 19:06:03 +0300

    [Atk] Properly hook into base method calls for Atk.Object.

--- atk/glue/object.c
+++ atk/glue/object.c
@@ -21,10 +21,29 @@
 
 #include <atk/atk.h>
 
+static const gchar *__prefix = "__gtksharp_";
+
+#define HAS_PREFIX(a) (*((guint64 *)(a)) == *((guint64 *) __prefix))
+
+static GObjectClass *
+get_threshold_class (GObject *obj)
+{
+	GType gtype = G_TYPE_FROM_INSTANCE (obj);
+	while (HAS_PREFIX (g_type_name (gtype)))
+		gtype = g_type_parent (gtype);
+	GObjectClass *klass = g_type_class_peek (gtype);
+	if (klass == NULL) klass = g_type_class_ref (gtype);
+	return klass;
+}
+
 
 void atksharp_object_override_get_n_children (GType gtype, gpointer cb);
 
+int atksharp_object_base_get_n_children (AtkObject *base);
+
 void atksharp_object_override_ref_child (GType gtype, gpointer cb);
+
+AtkObject *atksharp_object_base_ref_child (AtkObject *base, gint i);
 
 void atksharp_object_override_ref_state_set (GType gtype, gpointer cb);
 
@@ -32,9 +51,15 @@ AtkStateSet* atksharp_object_base_ref_state_set (AtkObject *base);
 
 void atksharp_object_override_get_index_in_parent (GType gtype, gpointer cb);
 
+gint atksharp_object_base_get_index_in_parent (AtkObject *base);
+
 void atksharp_object_override_ref_relation_set (GType gtype, gpointer cb);
 
 AtkRelationSet* atksharp_object_base_ref_relation_set (AtkObject *base);
+
+void atksharp_object_override_get_attributes(GType gtype, gpointer cb);
+
+AtkAttributeSet* atksharp_object_base_get_attributes (AtkObject *base);
 
 void
 atksharp_object_override_get_n_children (GType gtype, gpointer cb)
@@ -45,6 +70,15 @@ atksharp_object_override_get_n_children (GType gtype, gpointer cb)
 	((AtkObjectClass *) klass)->get_n_children = cb;
 }
 
+int
+atksharp_object_base_get_n_children (AtkObject *base)
+{
+	AtkObjectClass *parent = (AtkObjectClass *)get_threshold_class (G_OBJECT (base));
+	if (parent->get_n_children)
+		(*parent->get_n_children) (base);
+	return 0;
+}
+
 void
 atksharp_object_override_ref_child (GType gtype, gpointer cb)
 {
@@ -52,6 +86,15 @@ atksharp_object_override_ref_child (GType gtype, gpointer cb)
 	if (!klass)
 		klass = g_type_class_ref (gtype);
 	((AtkObjectClass *) klass)->ref_child = cb;
+}
+
+AtkObject *
+atksharp_object_base_ref_child (AtkObject *base, gint i)
+{
+	AtkObjectClass *parent = (AtkObjectClass *)get_threshold_class (G_OBJECT (base));
+	if (parent->ref_child)
+		(*parent->ref_child) (base, i);
+	return NULL;
 }
 
 void
@@ -66,8 +109,7 @@ atksharp_object_override_ref_state_set (GType gtype, gpointer cb)
 AtkStateSet*
 atksharp_object_base_ref_state_set (AtkObject *atk_obj)
 {
-	AtkObjectClass *parent = g_type_class_peek (ATK_TYPE_OBJECT);
-	
+	AtkObjectClass *parent = (AtkObjectClass *)get_threshold_class (G_OBJECT (atk_obj));
 	if (parent->ref_state_set)
 		return (*parent->ref_state_set) (atk_obj);
 	return NULL;
@@ -82,6 +124,15 @@ atksharp_object_override_get_index_in_parent (GType gtype, gpointer cb)
 	((AtkObjectClass *) klass)->get_index_in_parent = cb;
 }
 
+gint
+atksharp_object_base_get_index_in_parent (AtkObject *atk_obj)
+{
+	AtkObjectClass *parent = (AtkObjectClass *)get_threshold_class (G_OBJECT (atk_obj));
+	if (parent->get_index_in_parent)
+		return (*parent->get_index_in_parent) (atk_obj);
+	return -1;
+}
+
 void
 atksharp_object_override_ref_relation_set (GType gtype, gpointer cb)
 {
@@ -94,8 +145,7 @@ atksharp_object_override_ref_relation_set (GType gtype, gpointer cb)
 AtkRelationSet*
 atksharp_object_base_ref_relation_set (AtkObject *atk_obj)
 {
-	AtkObjectClass *parent = g_type_class_peek (ATK_TYPE_OBJECT);
-	
+	AtkObjectClass *parent = (AtkObjectClass *)get_threshold_class (G_OBJECT (atk_obj));
 	if (parent->ref_relation_set)
 		return (*parent->ref_relation_set) (atk_obj);
 	return NULL;
@@ -109,4 +159,13 @@ atksharp_object_override_get_attributes (GType gtype, gpointer cb)
 	if (!klass)
 		klass = g_type_class_ref (gtype);
 	((AtkObjectClass *) klass)->get_attributes = cb;
+}
+
+AtkAttributeSet*
+atksharp_object_base_get_attributes (AtkObject *base)
+{
+	AtkObjectClass *parent = (AtkObjectClass *)get_threshold_class (G_OBJECT (base));
+	if (parent->get_attributes)
+		return (*parent->get_attributes) (base);
+	return NULL;
 }
