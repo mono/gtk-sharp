39429e927173b489f382dfd8161a2b0fdd285b8b
Marius Ungureanu <marius.ungureanu@xamarin.com>
2017-05-28 19:51:50 +0300

    [GLib] Avoid locking in fastactivator by using threadstatic dictionaries.

--- glib/FastActivator.cs
+++ glib/FastActivator.cs
@@ -13,16 +13,14 @@ namespace GLib
 		static FastCreateObjectPtr FastCtorPtr (Type t, Dictionary<Type, FastCreateObjectPtr> cache)
 		{
 			FastCreateObjectPtr method;
-			lock (cache) {
-				if (!cache.TryGetValue (t, out method)) {
-					var param = Expression.Parameter (typeof (IntPtr));
-					var ctor = t.GetConstructor (flags, null, new [] { typeof (IntPtr) }, new ParameterModifier [0]);
-					if (ctor == null)
-						throw new MissingMethodException ();
+			if (!cache.TryGetValue (t, out method)) {
+				var param = Expression.Parameter (typeof (IntPtr));
+				var ctor = t.GetConstructor (flags, null, new [] { typeof (IntPtr) }, new ParameterModifier [0]);
+				if (ctor == null)
+					throw new MissingMethodException ();
 
-					var newExpr = Expression.New (ctor, param);
-					cache [t] = method = Expression.Lambda<FastCreateObjectPtr> (newExpr, param).Compile ();
-				}
+				var newExpr = Expression.New (ctor, param);
+				cache [t] = method = Expression.Lambda<FastCreateObjectPtr> (newExpr, param).Compile ();
 			}
 			return method;
 		}
@@ -31,11 +29,9 @@ namespace GLib
 		static FastCreateObject FastCtor (Type t, Dictionary<Type, FastCreateObject> cache)
 		{
 			FastCreateObject method;
-			lock (cache) {
-				if (!cache.TryGetValue (t, out method)) {
-					var newExpr = Expression.New (t);
-					cache [t] = method = Expression.Lambda<FastCreateObject> (newExpr).Compile ();
-				}
+			if (!cache.TryGetValue (t, out method)) {
+				var newExpr = Expression.New (t);
+				cache [t] = method = Expression.Lambda<FastCreateObject> (newExpr).Compile ();
 			}
 			return method;
 		}
@@ -44,40 +40,42 @@ namespace GLib
 		static FastCreateBoxed FastBoxed (Type t, Dictionary<Type, FastCreateBoxed> cache)
 		{
 			FastCreateBoxed method;
-			lock (cache) {
-				if (!cache.TryGetValue (t, out method)) {
-					var newMethod = t.GetMethod ("New", BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy);
-					if (newMethod != null) {
-						var param = Expression.Parameter (typeof (IntPtr));
-						var call = Expression.Call (newMethod, param);
-						var callWithConvert = Expression.Convert (call, typeof (object));
-						cache [t] = method = Expression.Lambda<FastCreateBoxed> (callWithConvert, param).Compile ();
-					} else {
-						cache [t] = method = ptr => System.Runtime.InteropServices.Marshal.PtrToStructure (ptr, t);
-					}
+			if (!cache.TryGetValue (t, out method)) {
+				var newMethod = t.GetMethod ("New", BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+				if (newMethod != null) {
+					var param = Expression.Parameter (typeof (IntPtr));
+					var call = Expression.Call (newMethod, param);
+					var callWithConvert = Expression.Convert (call, typeof (object));
+					cache [t] = method = Expression.Lambda<FastCreateBoxed> (callWithConvert, param).Compile ();
+				} else {
+					cache [t] = method = ptr => System.Runtime.InteropServices.Marshal.PtrToStructure (ptr, t);
 				}
 			}
 			return method;
 		}
 
+		[ThreadStatic]
 		static readonly Dictionary<Type, FastCreateObjectPtr> cacheOpaque = new Dictionary<Type, FastCreateObjectPtr> (new TypeEqualityComparer ());
 		public static Opaque CreateOpaque (IntPtr o, Type type)
 		{
 			return (Opaque)FastCtorPtr (type, cacheOpaque)(o);
 		}
 
+		[ThreadStatic]
 		static readonly Dictionary<Type, FastCreateObjectPtr> cacheObject = new Dictionary<Type, FastCreateObjectPtr> (new TypeEqualityComparer ());
 		public static Object CreateObject (IntPtr o, Type type)
 		{
 			return (Object)FastCtorPtr (type, cacheObject)(o);
 		}
 
+		[ThreadStatic]
 		static readonly Dictionary<Type, FastCreateObject> cacheSignalArgs = new Dictionary<Type, FastCreateObject> (new TypeEqualityComparer ());
 		public static SignalArgs CreateSignalArgs (Type type)
 		{
 			return (SignalArgs)FastCtor (type, cacheSignalArgs)();
 		}
 
+		[ThreadStatic]
 		static readonly Dictionary<Type, FastCreateBoxed> cacheBoxed = new Dictionary<Type, FastCreateBoxed> (new TypeEqualityComparer ());
 		public static object CreateBoxed (IntPtr o, Type type)
 		{
