f77d8d131fdfc7ed9663556e2f1e93ddebe359d3
Marius Ungureanu <marius.ungureanu@xamarin.com>
2017-06-01 00:08:52 +0300

    [GLib] Optimze Object memory retention.
    Only promote safehandle to finalizer.

--- glib/Object.cs
+++ glib/Object.cs
@@ -32,69 +32,26 @@ namespace GLib {
 	using System.Text;
 
 	public class Object : IWrapper, IDisposable {
-
-		IntPtr handle;
-		ToggleRef tref;
+		
 		internal protected bool owned = false;
 		Hashtable data;
-		static Dictionary<IntPtr, ToggleRef> Objects = new Dictionary<IntPtr, ToggleRef>(IntPtrEqualityComparer.Instance);
-		static object lockObject = new object ();
-		static List<ToggleRef> PendingDestroys = new List<ToggleRef> ();
-		static bool idle_queued;
-
-		~Object ()
-		{
-			lock (lockObject) {
-				lock (Objects) {
-					if (tref != null)
-						PendingDestroys.Add (tref);
-					Objects.Remove (Handle);
-				}
-				if (!idle_queued){
-					Timeout.Add (50, PerformQueuedUnrefs);
-					idle_queued = true;
-				}
-			}
-		}
+		internal PointerWrapper handle;
 
 		[DllImport("libgobject-2.0-0.dll", CallingConvention=CallingConvention.Cdecl)]
 		static extern void g_object_unref (IntPtr raw);
 
-		static bool PerformQueuedUnrefs ()
-		{
-			List<ToggleRef> references;
-
-			lock (lockObject) {
-				references = PendingDestroys;
-				PendingDestroys = new List<ToggleRef> ();
-				idle_queued = false;
-			}
-
-			foreach (ToggleRef r in references)
-				r.Free ();
-
-			return false;
-		}
-
 		public virtual void Dispose ()
 		{
-			if (Handle == IntPtr.Zero)
+			if (handle == null)
 				return;
-			
-			lock(Objects) {
-				Objects.Remove (Handle);
-				try {
-					if (tref != null) {
-						tref.Free ();
-						tref = null;
-					}
-				} catch (Exception e) {
-					Console.WriteLine ("Exception while disposing a " + this + " in Gtk#");
-					throw e;
-				}
+
+			try {
+				handle.Dispose ();
+				handle = null;
+			} catch (Exception e) {
+				Console.WriteLine ("Exception while disposing a " + this + " in Gtk#");
+				throw e;
 			}
-			handle = IntPtr.Zero;
-			GC.SuppressFinalize (this);
 		}
 
 		[DllImport("libgobject-2.0-0.dll", CallingConvention=CallingConvention.Cdecl)]
@@ -105,14 +62,9 @@ namespace GLib {
 			if (o == IntPtr.Zero)
 				return null;
 
-			ToggleRef tr;
-			lock(Objects)
-				Objects.TryGetValue (o, out tr);
-			if (tr != null) {
-				return tr.Target;
-			}
-
-			return null;
+			Object obj;
+			PointerWrapper.TryGetObject (o, out obj);
+			return obj;
 		}
 
 		public static Object GetObject(IntPtr o, bool owned_ref)
@@ -120,15 +72,9 @@ namespace GLib {
 			if (o == IntPtr.Zero)
 				return null;
 
-			Object obj = null;
-			ToggleRef toggle_ref = null;
-			lock(Objects)
-				Objects.TryGetValue (o, out toggle_ref);
+			Object obj;
 
-			if (toggle_ref != null)
-				obj = toggle_ref.Target;
-
-			if (obj != null && obj.Handle == o)
+			if (PointerWrapper.TryGetObject (o, out obj) && obj.Handle == o)
 				return obj;
 
 			obj = GLib.ObjectManager.CreateObject (o);
@@ -417,28 +363,18 @@ namespace GLib {
 
 		protected virtual IntPtr Raw {
 			get {
-				return handle;
+				return Handle;
 			}
 			set {
-				if (handle == value)
-					return;
+				if (handle != null) {
+					if (handle.handle == value)
+						return;
 
-				lock(Objects) {
-					if (handle != IntPtr.Zero) {
-						Objects.Remove (handle);
-						if (tref != null) {
-							tref.Free ();
-							tref = null;
-						}
-					}
-					handle = value;
-					if (value != IntPtr.Zero) {
-						tref = new ToggleRef (this);
-						Objects [value] = tref;
-					}
+					handle.Dispose ();
 				}
+				handle = PointerWrapper.Create (this, value);
 			}
-        	}
+		}
 
 		public static GLib.GType GType {
 			get {
@@ -463,20 +399,20 @@ namespace GLib {
 
 		internal ToggleRef ToggleRef {
 			get {
-				return tref;
+				return handle != null ? handle.tref : null;
 			}
 		}
 
 		public IntPtr Handle {
 			get {
-				return handle;
+				return handle != null ? handle.handle : IntPtr.Zero;
 			}
 		}
 
 		[DebuggerBrowsable (DebuggerBrowsableState.Never)]
 		public IntPtr OwnedHandle {
 			get {
-				return g_object_ref (handle);
+				return g_object_ref (Handle);
 			}
 		}
 
@@ -660,7 +596,7 @@ namespace GLib {
 
 		internal void Harden ()
 		{
-			tref.Harden ();
+			handle.tref.Harden ();
 		}
 
 		static Object ()
---
+++ glib/PointerWrapper.cs
@@ -0,0 +1,128 @@
+﻿// SafeObjectHandle.cs - SafeHandle implementation for GObject
+//
+// Authors: Marius Ungureanu <maungu@microsoft.com>
+//
+// Copyright (c) 2017-2017 Microsoft, Inc.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of version 2 of the Lesser GNU General
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, write to the
+// Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
+using System;
+using System.Collections.Generic;
+
+namespace GLib
+{
+	class PointerWrapper : IDisposable
+	{
+		internal ToggleRef tref;
+		internal IntPtr handle;
+
+		static Action<IntPtr> ObjectCreated;
+		static Action<IntPtr> ObjectDestroyed;
+
+		static readonly Dictionary<IntPtr, ToggleRef> Objects = new Dictionary<IntPtr, ToggleRef> (IntPtrEqualityComparer.Instance);
+
+		protected PointerWrapper (IntPtr handle)
+		{
+			this.handle = handle;
+		}
+
+		internal static PointerWrapper Create (Object obj, IntPtr handle)
+		{
+			if (handle == IntPtr.Zero)
+				return null;
+
+			PointerWrapper safeHandle;
+			safeHandle = new PointerWrapper (handle);
+			safeHandle.tref = new ToggleRef (obj, handle);
+
+			lock (Objects)
+				Objects [handle] = safeHandle.tref;
+			
+			if (ObjectCreated != null)
+				ObjectCreated.Invoke (handle);
+
+			return safeHandle;
+		}
+
+		internal static bool TryGetObject (IntPtr o, out Object obj)
+		{
+			bool ret;
+			ToggleRef tr;
+
+			lock (Objects)
+				ret = Objects.TryGetValue (o, out tr);
+
+			obj = ret ? tr.Target : null;
+			return obj != null;
+		}
+
+		static List<ToggleRef> PendingDestroys = new List<ToggleRef> ();
+		static readonly object lockObject = new object ();
+		static bool idle_queued;
+
+		static bool PerformQueuedUnrefs ()
+		{
+			List<ToggleRef> references;
+
+			lock (lockObject) {
+				references = PendingDestroys;
+				PendingDestroys = new List<ToggleRef>();
+				idle_queued = false;
+			}
+
+			foreach (ToggleRef r in references)
+				r.Free();
+
+			return false;
+		}
+
+		#region IDisposable
+		~PointerWrapper ()
+		{
+			lock (Objects)
+				Objects.Remove (handle);
+
+			if (ObjectDestroyed != null)
+				ObjectDestroyed.Invoke (handle);
+
+			lock (lockObject) {
+				if (tref != null) {
+					PendingDestroys.Add (tref);
+					if (!idle_queued) {
+						Timeout.Add (50, PerformQueuedUnrefs);
+						idle_queued = true;
+					}
+				}
+			}
+		}
+
+		public void Dispose ()
+		{
+			if (handle == IntPtr.Zero)
+				return;
+
+			GC.SuppressFinalize (this);
+			lock (Objects)
+				Objects.Remove (handle);
+
+			if (ObjectDestroyed != null)
+				ObjectDestroyed.Invoke (handle);
+			
+			handle = IntPtr.Zero;
+			tref.Free ();
+		}
+		#endregion
+	}
+}
