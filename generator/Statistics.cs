c3ab583f7c881997f9b1577d367fe8e5075fd072
Stephen Shaw <sshaw@decriptor.com>
2018-02-23 04:04:15 -0700

    Removed System.Collections from Statistics.cs

--- generator/Statistics.cs
+++ generator/Statistics.cs
@@ -19,12 +19,10 @@
 // Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 // Boston, MA 02111-1307, USA.
 
+using System;
 
 namespace GtkSharp.Generation {
-	
-	using System;
-	using System.Collections;
-	
+
 	public class Statistics {
 		
 		static int cbs = 0;
