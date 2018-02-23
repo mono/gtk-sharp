a53e2ca33eda88c7fc5a5fa4a07b1016aef24e77
Stephen Shaw <sshaw@decriptor.com>
2018-02-23 03:38:13 -0700

    Remove System.Collections from MethodBody.cs

--- generator/MethodBody.cs
+++ generator/MethodBody.cs
@@ -19,12 +19,10 @@
 // Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 // Boston, MA 02111-1307, USA.
 
+using System;
+using System.IO;
 
 namespace GtkSharp.Generation {
-
-	using System;
-	using System.Collections;
-	using System.IO;
 
 	public class MethodBody  {
 		
