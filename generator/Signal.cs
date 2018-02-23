6be4dc3455b4a65fb08f7a0c0fc4ea401c36a094
Stephen Shaw <sshaw@decriptor.com>
2018-02-23 04:02:04 -0700

    Remove System.Collections from Signal.cs

--- generator/Signal.cs
+++ generator/Signal.cs
@@ -20,13 +20,11 @@
 // Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 // Boston, MA 02111-1307, USA.
 
+using System;
+using System.IO;
+using System.Xml;
 
 namespace GtkSharp.Generation {
-
-	using System;
-	using System.Collections;
-	using System.IO;
-	using System.Xml;
 
 	public class Signal {
 
